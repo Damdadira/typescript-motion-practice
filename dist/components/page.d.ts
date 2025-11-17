a9a895715cd91abbaf03d1cff5ea1b40d62f87b8
duddk <duddk@scatterx.io>
2025-11-17 16:29:34 +0900

    feat: 레이아웃과 첫번째 page 컴포넌트 만들기2

---
+++ dist/components/page.d.ts
@@ -0,0 +1,6 @@
+export declare class PageComponent {
+    private element;
+    constructor();
+    attachTo(parent: HTMLElement, position?: InsertPosition): void;
+}
+//# sourceMappingURL=page.d.ts.map
