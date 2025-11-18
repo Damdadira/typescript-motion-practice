b9070f47d7796d1d58cc76dbaa5ee1de66176e22
duddk <duddk@scatterx.io>
2025-11-18 09:35:36 +0900

    feat: 이미지 컴포넌트 추가

--- dist/components/page.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class PageComponent {
-    private element;
-    constructor();
-    attachTo(parent: HTMLElement, position?: InsertPosition): void;
-}
-//# sourceMappingURL=page.d.ts.map
